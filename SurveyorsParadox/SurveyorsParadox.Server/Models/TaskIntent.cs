956a23e1fcfded8607e380035d502e96a4a7e827
HariHaran-Palanivelu <104771121+HariHaran-Palanivelu@users.noreply.github.com>
2024-09-24 16:34:27 +0530

    Task Intent
    Created new intent for the task creation

---
+++ SurveyorsParadox/SurveyorsParadox.Server/Models/TaskIntent.cs
@@ -0,0 +1,17 @@
+﻿namespace SurveyorsParadox.Server.Models
+{
+    public class TaskIntent
+    {
+        public string JobSiteName { get; set; }
+
+        public int Floor { get; set; }
+
+        public string Assignee { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string TaskType { get; set; }
+
+        public string TaskDescription { get; set; }
+    }
+}
