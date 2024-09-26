43e8c2bf13d8eacf91aa7af30d6566ca0bf701ac
vishal <gkvishal7@gmail.com>
2024-09-27 00:10:47 +0530

    Home Layout changes

--- SurveyorsParadox/surveyorsparadox.client/src/components/pages/HomePageComponent.tsx
+++ SurveyorsParadox/surveyorsparadox.client/src/components/pages/HomePageComponent.tsx
@@ -1,22 +1,17 @@
 import { Link } from 'react-router-dom';
-
+import { CardWithForm } from '../Card';
 const HomePageComponent: React.FC = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/form">Go to Form Page</Link>
-                    </li>
-                    <li>
-                        <Link to="/calendar">Go to Calendar</Link>
-                    </li>
-                    <li>
-                        <Link to="/reports">Go to Reports</Link>
-                    </li>
-                </ul>
-            </nav>
+        <div className='dark'>
+            <h1 className='text-center text-3xl my-10'>Home Page</h1>
+
+            <div className='m-auto p-28 flex flex-wrap justify-between'>
+                <CardWithForm title="Form" cardContent={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ratione laboriosam, iusto esse veniam ea corrupti laborum a quis? Distinctio?'} buttonContent="Go to Form" buttonURL={'/form'} ></CardWithForm>
+                <CardWithForm title="Calendar" cardContent={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ratione laboriosam, iusto esse veniam ea corrupti laborum a quis? Distinctio?'} buttonContent={'Go to Calendar'} buttonURL={'/calendar'}></CardWithForm>
+                <CardWithForm title="Reports" cardContent={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ratione laboriosam, iusto esse veniam ea corrupti laborum a quis? Distinctio?'} buttonContent={'Go to Reports'} buttonURL={'/reports'}></CardWithForm>
+            
+            </div>
+
         </div>
     );
 };
