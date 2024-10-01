811975653bb39e7f0f2817d8373e0ef00bfcc26c
vishal <gkvishal7@gmail.com>
2024-10-01 14:54:13 +0530

    dropdown & datePicker added

--- SurveyorsParadox/surveyorsparadox.client/src/components/TaskForm.tsx
+++ SurveyorsParadox/surveyorsparadox.client/src/components/TaskForm.tsx
@@ -1,40 +1,10 @@
 import DropdownMenu from "./DropdownMenu";
+import DatePicker  from "./DatePicker";
 
 export default function TaskForm(){
     return(
         <>
-        
-            {/* <div className="grid grid-cols-6 lvh">
-                <div className="col-sporder border-slate-200 3/4">
-                <div className="p-3 text-center align-middle border-2 border-b-slate-200">
-                    <div>JOB SITE</div>
-                </div>
-                <div className="p-3 text-center">
-                    <div>LOCATION</div>
-                </div>
-                <div className="p-3 text-center">
-                    <div>ASSIGNEE</div>
-                </div>
-                <div className="p-3 text-center">
-                    <div>TASK</div>
-                </div>
-                <div className="p-3 text-center">
-                    <div>DATE</div>
-                </div>
-                <div className="p-3 text-center">
-                    <div>DESCRIPTION</div>
-                </div>
-                    
-                    
-                   
-                </div>
 
-                <div className="col-span-5 border boder-2 border-slate-200 3/4">
-                
-                
-                </div>
-            </div> */}
-            
             <div className="m-12">
                 <div className="grid grid-cols-9">
                     <div className="col-span-2 border text-center p-10">
@@ -50,7 +20,7 @@ export default function TaskForm(){
                         <div>LOCATION</div>
                     </div>
                     <div className="col-span-7 border text-center p-10 ">
-                        <div>Dropdown Menu</div>
+                        <div><DropdownMenu/></div>
                     </div>
                 </div>
 
@@ -77,7 +47,7 @@ export default function TaskForm(){
                         <div>DATE</div>
                     </div>
                     <div className="col-span-7 border text-center p-10 ">
-                        <div>DATE PICKER</div>
+                        <div><DatePicker/></div>
                     </div>
                 </div>
 
@@ -89,8 +59,7 @@ export default function TaskForm(){
                         <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, sed maxime? Veniam obcaecati iste reprehenderit necessitatibus, deleniti culpa aliquid, voluptate maxime laboriosam quasi dignissimos in, neque earum minima. Dolor perspiciatis, expedita quasi dolore cupiditate itaque ducimus. Nam laboriosam est rerum hic dolor ipsa. Alias odit assumenda sequi pariatur maiores ullam?</div>
                     </div>
                 </div>
-
-                
+ 
             </div>
         
         
