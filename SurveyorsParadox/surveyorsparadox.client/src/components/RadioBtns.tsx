9ec9664681de8f7dd707bd9b14841e9684b4a84c
vishal <gkvishal7@gmail.com>
2024-10-01 16:30:37 +0530

    form updated

---
+++ SurveyorsParadox/surveyorsparadox.client/src/components/RadioBtns.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const options = [
+    "Option 1",
+    "Option 2",
+    "Option 3",
+    "Option 4"
+]
+
+export default function RadioBtns(){
+
+    const [selectedOption, setSelectedOption] = useState<string>("")
+    const [activeButton, setActiveButton] = useState<number | null>(null)
+
+    const handleChange = (event :React.ChangeEvent<HTMLInputElement>, index : number) =>{
+        setSelectedOption(event.target.value)
+        setActiveButton(index)
+       
+    }
+
+    return(
+        <>
+        
+            <div className="flex justify-around">
+                
+                {
+                    options.map((option :string, index: number) =>{
+                        return(
+                            <button 
+                            key={index}
+                            value={selectedOption}
+                            className={`py-2 px-10 border border-yellow-200 rounded-2xl ${index == activeButton ? "bg-yellow-300 text-black border-white" : ""}`}
+                            onClick={(e : React.ChangeEvent<HTMLInputElement>) => handleChange(e, index)}
+                            >{option}
+                            </button>
+                        )
+                    })
+                }
+            </div>
+
+        </>
+    );
+}
