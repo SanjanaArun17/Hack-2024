c7b887f974d8bd52347056b366ac1f46d2fa8484
Swaminathan1999 <swaminathanofficial@gmail.com>
2024-10-02 18:29:28 +0530

    Resolved entity connection
    Resolved entity connection

--- SurveyorsParadox/SurveyorsParadox.Server/Migrations/20241001060718_InitialCreateDb.Designer.cs
+++ SurveyorsParadox/SurveyorsParadox.Server/Migrations/20241001060718_InitialCreateDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SurveyorsParadox.Server.Database;
 
@@ -11,9 +12,11 @@ using SurveyorsParadox.Server.Database;
 namespace SurveyorsParadox.Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241001060718_InitialCreateDb")]
+    partial class InitialCreateDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -22,7 +25,7 @@ namespace SurveyorsParadox.Server.Migrations
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("SurveyorsParadox.Server.Database.TaskIntent", b =>
+            modelBuilder.Entity("SurveyorsParadox.Server.Database.TaskIntentEntity", b =>
                 {
                     b.Property<Guid>("TaskId")
                         .ValueGeneratedOnAdd()
