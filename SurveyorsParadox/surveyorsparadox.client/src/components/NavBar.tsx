225ec5d237d1280b3b9fd2eca6f5ce4bc06b18e8
vishal <gkvishal7@gmail.com>
2024-10-06 20:34:15 +0530

    navbar breakpoints added

--- SurveyorsParadox/surveyorsparadox.client/src/components/NavBar.tsx
+++ SurveyorsParadox/surveyorsparadox.client/src/components/NavBar.tsx
@@ -1,25 +1,67 @@
 import { Link, NavLink } from "react-router-dom";
-
+import { useState } from "react";
 export default function NavBar(){
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <>
-            <nav className="grid grid-cols-3 h-20 w-dvw border border-b-slate-100 items-center mb-12 font-semibold">
-                <div className="col-span-2 mx-10">
+        <div className="border border-b-slate-100">
+            <nav className="w-full flex justify-between h-20 font-semibold">
+                <div className="m-10 flex items-center">
                     <NavLink to="/" >OFFICE 2 FIELD</NavLink>
                 </div>
-                <div className="col-span-1 flex justify-around ">
+                 <div className={`w-40 justify-center flex items-center sm:hidden mx-5`}>
+                    <button
+                        className="text-white focus:outline-none"
+                        onClick={toggleMenu}
+                    >
+                        <svg
+                        className="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M4 6h16M4 12h16m-7 6h7"
+                        />
+                        </svg>
+                    </button>
+                </div>
+                <div className="hidden sm:flex items-center">
                     <div className="mx-4">
                         <NavLink to="/form" className={({isActive})=>`${ isActive? "text-yellow-400" : "text-white" }`}>ASSIGN TASK</NavLink>
                     </div>
                     <div className="mx-4">
                         <NavLink to="/calendar" className={({isActive})=>`${ isActive? "text-yellow-400" : "text-white" }`}>CALENDAR</NavLink>
                     </div>
-                    <div className="mx-4 text-yellow-200">
+                    <div className="mx-4">
                         <NavLink to="/reports" className={({isActive})=>`${ isActive? "text-yellow-400" : "text-white" }`}>REPORTS</NavLink>
                     </div>
                 </div>
+               
             </nav>
-        
-        </>
+
+            <div className="flex justify-end mr-7">
+                <div className={`overflow-hidden transition-max-height duration-300 ease-in-out ${isOpen ? 'max-h-48' : 'max-h-0'} sm:hidden`}>
+                    <div className="w-40 text-center my-2">
+                        <NavLink to="/form" className={({isActive})=>`${ isActive? "text-yellow-400" : "text-white" }`}>ASSIGN TASK</NavLink>
+                    </div>
+                    <div className="w-40 text-center my-2">
+                        <NavLink to="/calendar" className={({isActive})=>`${ isActive? "text-yellow-400" : "text-white" }`}>CALENDAR</NavLink>
+                    </div>
+                    <div className="w-40 text-center my-2">
+                        <NavLink to="/reports" className={({isActive})=>`${ isActive? "text-yellow-400" : "text-white" }`}>REPORTS</NavLink>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
