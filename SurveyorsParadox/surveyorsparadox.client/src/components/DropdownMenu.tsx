93cfa497edcc8e66f5a6f1422171cac7f626a1ae
vishal <gkvishal7@gmail.com>
2024-10-10 16:40:05 +0530

    task form theme changed

--- SurveyorsParadox/surveyorsparadox.client/src/components/DropdownMenu.tsx
+++ SurveyorsParadox/surveyorsparadox.client/src/components/DropdownMenu.tsx
@@ -56,7 +56,7 @@ export default function DropdownMenu() {
 			<div className="flex justify-center">
 				
 				<button id="dropdownButton" 
-				className="border border-yellow-200 rounded-lg p-2 w-2/3 text-left flex justify-between items-center focus:outline-none"
+				className="border-2 border-customBlue rounded-lg p-2 w-2/3 text-left flex justify-between items-center focus:outline-none"
 				ref={buttonRef}
 				onClick={() => setOpen(!open)}
 				>
@@ -66,12 +66,12 @@ export default function DropdownMenu() {
 			</div>
 			<div className="flex justify-center">
 
-				<div id="dropdownMenu"ref={dropDownRef}  className={`absolute z-10 ${open ? "": "hidden" } w-2/3 border border-yellow-200 rounded-lg mt-1 bg-black text-white shadow-lg`}>
+				<div id="dropdownMenu"ref={dropDownRef}  className={`absolute z-10 ${open ? "": "hidden" } w-2/3 border border-customBlue rounded-lg mt-1 bg-white shadow-lg`}>
 					<div className="max-h-60 overflow-auto">
 						{
 							frameworks.map((val)=>{
 								return (
-									<div className="p-2 hover:border hover:border-yellow-100 cursor-pointer" key={val.value} onClick={()=> selectOption(val.value)} >{ val.label }</div>
+									<div className="p-2 hover:border hover:border-customBlue cursor-pointer" key={val.value} onClick={()=> selectOption(val.value)} >{ val.label }</div>
 								)
 							})
 						}
